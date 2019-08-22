cbe8f6c38b7efb0b0f7dcb7878e6d370c4b7a70b
alumno <alumno@E3MAESTRO>
2019-08-22 11:45:33 -0700

    eje x

--- GraficadorSeñales/MainWindow.xaml.cs
+++ GraficadorSeñales/MainWindow.xaml.cs
@@ -56,15 +56,25 @@ namespace GraficadorSeñales
             {
                 plnGrafica.Points.Add(
                     adaptarCoordenadas(i,
-                    señal.evaluar(i))
+                    señal.evaluar(i), tiempoInicial)
                     );
             }
+
+            plnEjeX.Points.Clear();
+            plnEjeX.Points.Add(
+                adaptarCoordenadas(tiempoInicial,0.0,
+                tiempoInicial)
+                );
+            plnEjeX.Points.Add(
+                adaptarCoordenadas(tiempoFinal,0.0, tiempoInicial)
+                );
+
         }
 
         public Point adaptarCoordenadas(
-            double x, double y)
+            double x, double y, double tiempoInicial)
         {
-            return new Point(x * scrGrafica.Width,
+            return new Point((x - tiempoInicial) * scrGrafica.Width,
                 (-1 * (
                 y * ((scrGrafica.Height /2.0) -25 ) )) +
                 (scrGrafica.Height / 2.0) );
